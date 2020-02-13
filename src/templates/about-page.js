12864982a9526eb2cbf8a810020e50736e00c819
Craig Booker <craig@arcadiasoftworks.com>
2020-02-13 13:11:16 -0600

    Made a layout change to the about page.

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,62 +2,248 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
+import Pricing from '../components/Pricing'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+export const AboutPageTemplate = ({
+	image,
+	title,
+	heading,
+	description,
+	intro,
+	main,
+	testimonials,
+	fullImage,
+	pricing
+}) => (
+	<div className="content">
+		<div
+			className="full-width-image-container margin-top-0"
+			style={{
+				backgroundImage: `url(${
+					!!image.childImageSharp ? image.childImageSharp.fluid.src : image
+				})`
+			}}
+		>
+			<h2
+				className="has-text-weight-bold is-size-1"
+				style={{
+					boxShadow:
+						'rgb(0, 186, 187, 0.75) 0.5rem 0px 0px, rgb(0, 186, 187, 0.75) -0.5rem 0px 0px',
+					backgroundColor: 'rgb(0, 186, 187, 0.75)',
+					color: 'white',
+					lineHeight: '1',
+					padding: '0.25em'
+				}}
+			>
+				{title}
+			</h2>
+		</div>
+		<section className="section section--gradient">
+			<div className="container">
+				<div className="section">
+					<div className="columns">
+						<div className="column is-7 is-offset-1">
+							<h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+							<p>{description}</p>
+						</div>
+					</div>
+					<div className="columns">
+						<div className="column is-10 is-offset-1">
+							<Features gridItems={intro.blurbs} />
+							<div className="columns">
+								<div className="column is-7">
+									<h3 className="has-text-weight-semibold is-size-3">
+										{main.heading}
+									</h3>
+									<p>{main.description}</p>
+								</div>
+							</div>
+							<div className="tile is-ancestor">
+								<div className="tile is-vertical">
+									<div className="tile">
+										<div className="tile is-parent is-vertical">
+											<article className="tile is-child">
+												<PreviewCompatibleImage imageInfo={main.image1} />
+											</article>
+										</div>
+										<div className="tile is-parent">
+											<article className="tile is-child">
+												<PreviewCompatibleImage imageInfo={main.image2} />
+											</article>
+										</div>
+									</div>
+									<div className="tile is-parent">
+										<article className="tile is-child">
+											<PreviewCompatibleImage imageInfo={main.image3} />
+										</article>
+									</div>
+								</div>
+							</div>
+							<Testimonials testimonials={testimonials} />
+							<div
+								className="full-width-image-container"
+								style={{
+									backgroundImage: `url(${
+										fullImage.childImageSharp
+											? fullImage.childImageSharp.fluid.src
+											: fullImage
+									})`
+								}}
+							/>
+							<h2 className="has-text-weight-semibold is-size-2">
+								{pricing.heading}
+							</h2>
+							<p className="is-size-5">{pricing.description}</p>
+							<Pricing data={pricing.plans} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+)
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+	title: PropTypes.string,
+	heading: PropTypes.string,
+	description: PropTypes.string,
+	intro: PropTypes.shape({
+		blurbs: PropTypes.array
+	}),
+	main: PropTypes.shape({
+		heading: PropTypes.string,
+		description: PropTypes.string,
+		image1: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+		image2: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+		image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+	}),
+	testimonials: PropTypes.array,
+	fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+	pricing: PropTypes.shape({
+		heading: PropTypes.string,
+		description: PropTypes.string,
+		plans: PropTypes.array
+	})
 }
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+	const { frontmatter } = data.markdownRemark
 
-  return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
-  )
+	return (
+		<Layout>
+			<AboutPageTemplate
+				image={frontmatter.image}
+				title={frontmatter.title}
+				heading={frontmatter.heading}
+				description={frontmatter.description}
+				intro={frontmatter.intro}
+				main={frontmatter.main}
+				testimonials={frontmatter.testimonials}
+				fullImage={frontmatter.full_image}
+				pricing={frontmatter.pricing}
+			/>
+		</Layout>
+	)
 }
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.shape({
+			frontmatter: PropTypes.object
+		})
+	})
 }
 
 export default AboutPage
 
 export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
+	query AboutPage($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			frontmatter {
+				title
+				image {
+					childImageSharp {
+						fluid(maxWidth: 2048, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				heading
+				description
+				intro {
+					blurbs {
+						image {
+							childImageSharp {
+								fluid(maxWidth: 240, quality: 64) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+						text
+					}
+					heading
+					description
+				}
+				main {
+					heading
+					description
+					image1 {
+						alt
+						image {
+							childImageSharp {
+								fluid(maxWidth: 526, quality: 92) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+					image2 {
+						alt
+						image {
+							childImageSharp {
+								fluid(maxWidth: 526, quality: 92) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+					image3 {
+						alt
+						image {
+							childImageSharp {
+								fluid(maxWidth: 1075, quality: 72) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+				}
+				testimonials {
+					author
+					quote
+				}
+				full_image {
+					childImageSharp {
+						fluid(maxWidth: 2048, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				pricing {
+					heading
+					description
+					plans {
+						description
+						items
+						plan
+						price
+					}
+				}
+			}
+		}
+	}
 `
