a1da88202c0352a31f42eea5f83bbd12a362b13e
Craig Booker <craig@arcadiasoftworks.com>
2020-02-18 10:33:13 -0600

    Added basic framework for posting projects or portfolio items. This includes the page, the post template, the component. Needs work.

---
+++ src/pages/portfolio/sample-portfolio-post.md
@@ -0,0 +1,12 @@
+---
+templateKey: 'portfolio-post'
+title: 'Sample Project Post'
+date: '2020-02-18'
+featuredpost: false
+featuredimage: /img/chemex.jpg
+description:
+tags:
+  - Web
+---
+
+Text goes here...
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
-import FeaturedPosts from '../components/FeaturedPosts'
+import Layout from '../components/Layout';
+import Features from '../components/Features';
+import FeaturedPosts from '../components/FeaturedPosts';
 
 export const IndexPageTemplate = ({
 	image,
@@ -18,7 +17,7 @@ export const IndexPageTemplate = ({
 }) => (
 	<div>
 		<div
-			className="full-width-image margin-top-0"
+			className='full-width-image margin-top-0'
 			style={{
 				backgroundImage: `url(${
 					!!image.childImageSharp ? image.childImageSharp.fluid.src : image
@@ -38,7 +37,7 @@ export const IndexPageTemplate = ({
 				}}
 			>
 				<h1
-					className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+					className='has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen'
 					style={{
 						boxShadow:
 							'rgb(0, 186, 187, 0.75) 0.5rem 0px 0px, rgb(0, 186, 187, 0.75) -0.5rem 0px 0px',
@@ -51,7 +50,7 @@ export const IndexPageTemplate = ({
 					{title}
 				</h1>
 				<h3
-					className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+					className='has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen'
 					style={{
 						boxShadow:
 							'rgb(0, 186, 187, 0.75) 0.5rem 0px 0px, rgb(0, 186, 187, 0.75) -0.5rem 0px 0px',
@@ -65,43 +64,43 @@ export const IndexPageTemplate = ({
 				</h3>
 			</div>
 		</div>
-		<section className="section section--gradient">
-			<div className="container">
-				<div className="section">
-					<div className="columns">
-						<div className="column is-10 is-offset-1">
-							<div className="content">
-								<div className="content">
-									<div className="tile">
-										<h1 className="title">{mainpitch.title}</h1>
+		<section className='section section--gradient'>
+			<div className='container'>
+				<div className='section'>
+					<div className='columns'>
+						<div className='column is-10 is-offset-1'>
+							<div className='content'>
+								<div className='content'>
+									<div className='tile'>
+										<h1 className='title'>{mainpitch.title}</h1>
 									</div>
-									<div className="tile">
-										<h3 className="subtitle">{mainpitch.description}</h3>
+									<div className='tile'>
+										<h3 className='subtitle'>{mainpitch.description}</h3>
 									</div>
 								</div>
-								<div className="columns">
-									<div className="column is-12">
-										<h3 className="has-text-weight-semibold is-size-2">
+								<div className='columns'>
+									<div className='column is-12'>
+										<h3 className='has-text-weight-semibold is-size-2'>
 											{heading}
 										</h3>
 										<p>{description}</p>
 									</div>
 								</div>
 								<Features gridItems={intro.blurbs} />
-								<div className="columns">
-									<div className="column is-12 has-text-centered">
-										<Link className="btn" to="/apps">
+								<div className='columns'>
+									<div className='column is-12 has-text-centered'>
+										<Link className='btn' to='/apps'>
 											See all apps
 										</Link>
 									</div>
 								</div>
-								<div className="column is-12">
-									<h3 className="has-text-weight-semibold is-size-2">
+								<div className='column is-12'>
+									<h3 className='has-text-weight-semibold is-size-2'>
 										Latest stories
 									</h3>
 									<FeaturedPosts />
-									<div className="column is-12 has-text-centered">
-										<Link className="btn" to="/blog">
+									<div className='column is-12 has-text-centered'>
+										<Link className='btn' to='/blog'>
 											Read more
 										</Link>
 									</div>
@@ -113,7 +112,7 @@ export const IndexPageTemplate = ({
 			</div>
 		</section>
 	</div>
-)
+);
 
 IndexPageTemplate.propTypes = {
 	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -125,10 +124,10 @@ IndexPageTemplate.propTypes = {
 	intro: PropTypes.shape({
 		blurbs: PropTypes.array
 	})
-}
+};
 
 const IndexPage = ({ data }) => {
-	const { frontmatter } = data.markdownRemark
+	const { frontmatter } = data.markdownRemark;
 
 	return (
 		<Layout>
@@ -142,8 +141,8 @@ const IndexPage = ({ data }) => {
 				intro={frontmatter.intro}
 			/>
 		</Layout>
-	)
-}
+	);
+};
 
 IndexPage.propTypes = {
 	data: PropTypes.shape({
@@ -151,9 +150,9 @@ IndexPage.propTypes = {
 			frontmatter: PropTypes.object
 		})
 	})
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
 	query IndexPageTemplate {
@@ -191,4 +190,4 @@ export const pageQuery = graphql`
 			}
 		}
 	}
-`
+`;
