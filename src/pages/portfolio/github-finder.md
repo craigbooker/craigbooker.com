4cefb49dfe3cf95f3ca342ba270cd12f732f24d6
Craig Booker <craig@arcadiasoftworks.com>
2020-03-06 01:06:48 -0600

    Added a new project and another blog post.

---
+++ src/pages/portfolio/github-finder.md
@@ -0,0 +1,17 @@
+---
+templateKey: 'portfolio-post'
+title: 'Github Finder'
+featuredimage: /img/projects/github-finder.png
+date: '2020-03-05'
+featuredpost: false
+description:
+tags:
+  - Web
+---
+
+![Github Finder](/img/projects/github-finder.png)
+
+**Description:**
+A react app which utilizes the Github API to search for Github users and display their profile along with their public repos.
+
+[Github Finder](https://githubfinder0243.netlify.com/)
--- src/templates/portfolio-post.js
+++ src/templates/portfolio-post.js
@@ -5,44 +5,118 @@ import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+import craigAvatar from '../../static/img/craig-booker.jpg';
+import tagsSVG from '../img/fa-tags.svg';
 
 export const PortfolioPostTemplate = ({
 	content,
 	contentComponent,
 	description,
+	image,
+	readingtime,
+	wordcount,
+	slug,
+	date,
 	tags,
 	title,
+	frontmatter,
 	helmet
 }) => {
 	const PortfolioContent = contentComponent || Content;
 
 	return (
-		<section className='section'>
+		<div className='portfolio-post'>
 			{helmet || ''}
-			<div className='container content'>
-				<div className='columns'>
-					<div className='column is-10 is-offset-1'>
-						<h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
-							{title}
-						</h1>
-						<p>{description}</p>
-						<PortfolioContent content={content} />
-						{tags && tags.length ? (
-							<div style={{ marginTop: `4rem` }}>
-								<h4>Tags</h4>
-								<ul className='taglist'>
-									{tags.map(tag => (
-										<li key={tag + `tag`}>
-											<Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-										</li>
-									))}
-								</ul>
-							</div>
-						) : null}
+			<div
+				className='full-width-image margin-top-0'
+				style={{
+					backgroundImage: `url(${
+						!!image.childImageSharp ? image.childImageSharp.fluid.src : image
+					})`,
+					backgroundPosition: `top left`,
+					backgroundAttachment: `fixed`
+				}}
+			>
+				<div className='blog-title-wrap'>
+					<h1 className='has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen'>
+						{title}
+					</h1>
+					<div className='has-text-centered;'>
+						<span id='publish-date'>
+							<span aria-labelledby='publish-date' role='img'>
+								📅
+							</span>{' '}
+							Published on {date}
+						</span>{' '}
+						by Craig Booker{' '}
+						<span id='reading-time'>
+							<span aria-labelledby='reading-time' role='img'>
+								🕑
+							</span>{' '}
+							{readingtime}
+						</span>{' '}
+						<span id='wordcount'>
+							<span aria-labelledby='wordcount' role='img'>
+								🖹
+							</span>{' '}
+							{wordcount} words
+						</span>
 					</div>
 				</div>
 			</div>
-		</section>
+			<div className='container content article'>
+				<div className='columns'>
+					<div className='column is-10 is-offset-1'>
+						<article className='article-body column is-8 is-child'>
+							<PortfolioContent content={content} />
+						</article>
+						<aside className='column is-child'>
+							<div className='aside-wrap'>
+								<div className='author-card--sidebar box'>
+									<div className='author-card-image'>
+										<img src={craigAvatar} alt='Craig Booker' />
+									</div>
+									<div className='author-card-desc'>
+										<strong>About the Author</strong>
+										<p>
+											<a href='https://zerotodev.net/about/' rel='me'>
+												Craig Booker
+											</a>{' '}
+											is an iOS Developer and Writer .
+										</p>
+									</div>
+								</div>
+								{tags && tags.length ? (
+									<div
+										className='blog-categories box'
+										style={{ marginTop: `2rem` }}
+									>
+										<h4>Categories</h4>
+										<ul className='taglist'>
+											{tags.map(tag => (
+												<li key={tag + `tag`}>
+													<Link to={`/tags/${kebabCase(tag)}/`}>
+														<span>
+															<object
+																type='image/svg+xml'
+																aria-label='Post Tag'
+																className='social-svg'
+																data={tagsSVG}
+															/>
+														</span>{' '}
+														{tag}
+													</Link>
+												</li>
+											))}
+										</ul>
+									</div>
+								) : null}
+							</div>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
@@ -50,7 +124,9 @@ PortfolioPostTemplate.propTypes = {
 	content: PropTypes.node.isRequired,
 	contentComponent: PropTypes.func,
 	description: PropTypes.string,
+	frontmatter: PropTypes.object,
 	title: PropTypes.string,
+	date: PropTypes.string,
 	helmet: PropTypes.object
 };
 
@@ -62,7 +138,15 @@ const PortfolioPost = ({ data }) => {
 			<PortfolioPostTemplate
 				content={post.html}
 				contentComponent={HTMLContent}
+				date={post.frontmatter.date}
 				description={post.frontmatter.description}
+				frontmatter={post.frontmatter}
+				tags={post.frontmatter.tags}
+				title={post.frontmatter.title}
+				image={post.frontmatter.featuredimage}
+				wordcount={post.fields.readingTime.words}
+				slug={post.fields.slug}
+				readingtime={post.fields.readingTime.text}
 				helmet={
 					<Helmet titleTemplate='%s | Blog'>
 						<title>{`${post.frontmatter.title}`}</title>
@@ -70,10 +154,13 @@ const PortfolioPost = ({ data }) => {
 							name='description'
 							content={`${post.frontmatter.description}`}
 						/>
+						<meta name='og:title' content={`${post.frontmatter.title}`} />
+						<meta
+							name='og:description'
+							content={`${post.frontmatter.description}`}
+						/>
 					</Helmet>
 				}
-				tags={post.frontmatter.tags}
-				title={post.frontmatter.title}
 			/>
 		</Layout>
 	);
@@ -92,8 +179,24 @@ export const pageQuery = graphql`
 		markdownRemark(id: { eq: $id }) {
 			id
 			html
+			fields {
+				slug
+				readingTime {
+					text
+					words
+				}
+			}
 			frontmatter {
 				date(formatString: "MMMM DD, YYYY")
+				featuredimage {
+					publicURL
+					childImageSharp {
+						fluid(maxWidth: 1280, quality: 75) {
+							...GatsbyImageSharpFluid_withWebp
+						}
+					}
+				}
+
 				title
 				description
 				tags
