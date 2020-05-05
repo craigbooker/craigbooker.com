5af6d85dde91d478bd07c5705ccf2403c07a2b98
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-05-05 11:29:10 -0500

    Added a few plugins for seo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,32 +5,40 @@ module.exports = {
 		title: 'The Official site of Craig Booker',
 		author: config.author,
 		description: config.defaultDescription,
-		siteUrl: config.url
+		siteUrl: config.url,
 	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sass',
+		'gatsby-plugin-robots-txt',
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// replace "UA-XXXXXXXXX-X" with your own Tracking ID
+				trackingId: 'UA-127519592-2',
+			},
+		},
 		{
 			// keep as first gatsby-source-filesystem plugin for gatsby image support
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				path: `${__dirname}/static/img`,
-				name: 'uploads'
-			}
+				name: 'uploads',
+			},
 		},
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				path: `${__dirname}/src/pages`,
-				name: 'pages'
-			}
+				name: 'pages',
+			},
 		},
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				path: `${__dirname}/src/img`,
-				name: 'images'
-			}
+				name: 'images',
+			},
 		},
 		'gatsby-plugin-sharp',
 		'gatsby-transformer-sharp',
@@ -42,8 +50,8 @@ module.exports = {
 					{
 						resolve: 'gatsby-remark-relative-images',
 						options: {
-							name: 'uploads'
-						}
+							name: 'uploads',
+						},
 					},
 					{
 						resolve: 'gatsby-remark-images',
@@ -51,31 +59,31 @@ module.exports = {
 							// It's important to specify the maxWidth (in pixels) of
 							// the content container as this plugin uses this as the
 							// base for generating different widths of each image.
-							maxWidth: 2048
-						}
+							maxWidth: 2048,
+						},
 					},
 					{
 						resolve: 'gatsby-remark-copy-linked-files',
 						options: {
-							destinationDir: 'static'
-						}
-					}
-				]
-			}
+							destinationDir: 'static',
+						},
+					},
+				],
+			},
 		},
 		{
 			resolve: 'gatsby-plugin-netlify-cms',
 			options: {
-				modulePath: `${__dirname}/src/cms/cms.js`
-			}
+				modulePath: `${__dirname}/src/cms/cms.js`,
+			},
 		},
 		{
 			resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
 			options: {
 				develop: true, // Activates purging in npm run develop
-				purgeOnly: ['/all.sass'] // applies purging only on the bulma css file
-			}
+				purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+			},
 		}, // must be after other CSS plugins
-		'gatsby-plugin-netlify' // make sure to keep it last in the array
-	]
+		'gatsby-plugin-netlify', // make sure to keep it last in the array
+	],
 };
