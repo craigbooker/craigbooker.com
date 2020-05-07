2f03f5aea45da9e7d16e133cd168e3d8e20a0540
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-05-07 13:11:28 -0500

    Changed the graphql query to use async await. Added some functionality for pagination of posts.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,55 @@
-const _ = require('lodash')
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const _ = require('lodash');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+exports.createPages = async ({ actions, graphql, reporter }) => {
+	const { createPage } = actions;
 
-  return graphql(`
-    {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-            frontmatter {
-              tags
-              templateKey
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
-    }
+	const result = await graphql(`
+		{
+			allMarkdownRemark(limit: 1000) {
+				edges {
+					node {
+						id
+						fields {
+							slug
+						}
+						frontmatter {
+							tags
+							templateKey
+						}
+					}
+				}
+			}
+		}
+	`);
 
-    const posts = result.data.allMarkdownRemark.edges
+	if (result.errors) {
+		reporter.panicOnBuild('Error while running GraphQL query.');
+		return;
+	}
 
+	// Create blog-list pages
+	const posts = result.data.allMarkdownRemark.edges;
+	const postsPerPage = 4;
+	const numPages = Math.ceil(posts.length / postsPerPage);
+	Array.from({ length: numPages }).forEach((_, i) => {
+		createPage({
+			path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+			component: path.resolve('./src/templates/blog-list.js'),
+			// additional data can be passed via context
+			context: {
+				limit: postsPerPage,
+				skip: i * postsPerPage,
+				numPages,
+				currentPage: i + 1,
+			},
+		});
+		//  })
+		//}
+		/*     const posts = result.data.allMarkdownRemark.edges
+    
     posts.forEach(edge => {
       const id = edge.node.id
       createPage({
@@ -44,44 +63,44 @@ exports.createPages = ({ actions, graphql }) => {
           id,
         },
       })
-    })
+    }) */
 
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
+		// Tag pages:
+		let tags = [];
+		// Iterate through each post, putting all found tags into `tags`
+		posts.forEach((edge) => {
+			if (_.get(edge, `node.frontmatter.tags`)) {
+				tags = tags.concat(edge.node.frontmatter.tags);
+			}
+		});
+		// Eliminate duplicate tags
+		tags = _.uniq(tags);
 
-    // Make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
+		// Make tag pages
+		tags.forEach((tag) => {
+			const tagPath = `/tags/${_.kebabCase(tag)}/`;
 
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
-    })
-  })
-}
+			createPage({
+				path: tagPath,
+				component: path.resolve(`src/templates/tags.js`),
+				context: {
+					tag,
+				},
+			});
+		});
+	});
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
+	const { createNodeField } = actions;
+	fmImagesToRelative(node); // convert image paths for gatsby images
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+	if (node.internal.type === `MarkdownRemark`) {
+		const value = createFilePath({ node, getNode });
+		createNodeField({
+			name: `slug`,
+			node,
+			value,
+		});
+	}
+};
