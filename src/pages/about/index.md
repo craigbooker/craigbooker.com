5812906b2d57151fa86465a13ae92aea8341ec97
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-06-02 10:48:40 -0500

    Moved apps into about page.

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -8,3 +8,9 @@ description: >-
   I'm a Writer and Software Developer  from Oklahoma City, Oklahoma.  
   I started tinkering with electronics when I was young. I first learned about repairing Apple(iOS) devices when I worked for Apple retail. It was at Apple retail I received my certification to work on what Apple calls small device repair. At that time this included iPhones, iPads, and iPods. I also learned Apple’s methods in training individuals to get the most out of their products. I aim to take what I learned at Apple and combine the best customer service, my love for great apps and my dedication to my customers to provide the best products. It is at the intersection of these three areas where I shine the best.
 ---
+
+## Pure Trivia
+
+![](/img/pureTriviaAppPromo.png)
+
+A Bible-based trivia game to encourage discussion between parents and kids regarding what the Bible says about everyday topics
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -4,53 +4,68 @@ import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import PageHeader from '../components/PageHeader';
+import Content, { HTMLContent } from '../components/Content';
 import FeaturedPortfolio from '../components/FeaturedPortfolio';
 
 export const AboutPageTemplate = ({
+	content,
+	contentComponent,
 	image,
 	title,
 	heading,
 	subheading,
 	description,
-}) => (
-	<div>
-		<PageHeader title={title} subheading={subheading} image={image} />
-		<section className='section section--gradient'>
-			<div className='container'>
-				<div className='section'>
-					<div className='columns'>
-						<div className='column is-10 is-offset-1'>
-							<div className='content'>
-								<div className='columns'>
+}) => {
+	const PostContent = contentComponent || Content;
+
+	return (
+		<div>
+			<PageHeader title={title} subheading={subheading} image={image} />
+			<section className='section section--gradient'>
+				<div className='container'>
+					<div className='section'>
+						<div className='columns'>
+							<div className='column is-10 is-offset-1'>
+								<div className='content'>
+									<div className='columns'>
+										<div className='column is-12'>
+											<h3 className='has-text-weight-semibold is-size-2'>
+												{heading}
+											</h3>
+											<p>{description}</p>
+										</div>
+									</div>
+
 									<div className='column is-12'>
 										<h3 className='has-text-weight-semibold is-size-2'>
-											{heading}
+											Recent Projects
 										</h3>
-										<p>{description}</p>
+										<FeaturedPortfolio />
+										<div className='column is-12 has-text-centered'>
+											<Link className='btn' to='/portfolio'>
+												See more projects
+											</Link>
+										</div>
 									</div>
-								</div>
-
-								<div className='column is-12'>
-									<h3 className='has-text-weight-semibold is-size-2'>
-										Recent Projects
-									</h3>
-									<FeaturedPortfolio />
-									<div className='column is-12 has-text-centered'>
-										<Link className='btn' to='/portfolio'>
-											See more projects
-										</Link>
+									<div className='column is-12'>
+										<br />
+										<br />
+										<h3 className='has-text-weight-semibold is-size-2'>Apps</h3>
+										<PostContent content={content} />
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	</div>
-);
+			</section>
+		</div>
+	);
+};
 
 AboutPageTemplate.propTypes = {
+	content: PropTypes.node.isRequired,
+	contentComponent: PropTypes.func,
 	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 	title: PropTypes.string,
 	heading: PropTypes.string,
@@ -60,10 +75,12 @@ AboutPageTemplate.propTypes = {
 
 const AboutPage = ({ data }) => {
 	const { frontmatter } = data.markdownRemark;
-
+	const { markdownRemark: aboutPost } = data;
 	return (
 		<Layout>
 			<AboutPageTemplate
+				content={aboutPost.html}
+				contentComponent={HTMLContent}
 				image={frontmatter.image}
 				title={frontmatter.title}
 				heading={frontmatter.heading}
@@ -76,9 +93,7 @@ const AboutPage = ({ data }) => {
 
 AboutPage.propTypes = {
 	data: PropTypes.shape({
-		markdownRemark: PropTypes.shape({
-			frontmatter: PropTypes.object,
-		}),
+		markdownRemark: PropTypes.object,
 	}),
 };
 
@@ -87,6 +102,7 @@ export default AboutPage;
 export const aboutPageQuery = graphql`
 	query AboutPageTemplate {
 		markdownRemark(frontmatter: { templateKey: { eq: "about-page" } }) {
+			html
 			frontmatter {
 				title
 				image {
